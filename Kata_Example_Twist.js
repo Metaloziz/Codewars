f126b1a8d5ec7a5b6dbd40a94616a2dc1ff3735a
Andrew Gaityukevich <AndrewGaity@yandex.by>
2021-10-17 00:23:22 +0300

    Create  Kata_Example_Twist.js
    This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
    
    Add the value "codewars" to the array websites/Websites 1,000 times.
    
    var websites = []

---
+++ Kata_Example_Twist.js
@@ -0,0 +1,10 @@
+var websites = [] 
+        function b () {  
+             
+            for (let i = 0; i < 1000; i++) {
+                websites.push("codewars")
+            }
+            return b.legth
+        } 
+b()
+       
